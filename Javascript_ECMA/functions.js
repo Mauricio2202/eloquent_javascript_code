30af1d78d8c1966354831f6c7dad82c12df092e9
Omega <elbackenddev22@gmail.com>
2025-03-04 19:42:49 -0600

    funciones-potencia

--- Javascript_ECMA/functions.js
+++ Javascript_ECMA/functions.js
@@ -3,3 +3,19 @@ const cuadrado = function(x) {
 };
 
 console.log(cuadrado(12))
+
+const hacerSonido = function() {
+    console.log("Pling!");
+}
+
+hacerSonido();
+
+const potencia = function(base, exponente) {
+    let resultado = 1;
+    for (let cuenta = 0; cuenta < exponente; cuenta++) {
+        resultado *= base;
+    }
+    return resultado;
+};
+
+console.log(potencia(2, 10));
