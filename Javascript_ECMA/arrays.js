5d40dc65744e0478a916b0d8e64bc5cce393d9fb
Omega <elbackenddev22@gmail.com>
2025-03-08 13:35:40 -0600

    Arrays inicio

---
+++ Javascript_ECMA/arrays.js
@@ -0,0 +1,16 @@
+let listaDeNumeros = [2, 3, 5, 7, 11];
+console.log(listaDeNumeros[2]);
+console.log(listaDeNumeros[0]);
+console.log(listaDeNumeros[2 -1]);
+
+let ouch = "Ouch";
+console.log(typeof ouch.toUpperCase);
+console.log(ouch.toUpperCase());
+
+let secuencia = [1, 2, 3];
+secuencia.push(4);
+secuencia.push(5);
+console.log(secuencia);
+
+console.log(secuencia.pop());
+console.log(secuencia);
