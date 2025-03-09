c3da580b37d903b872a795a10368644a7c3fb65e
Omega <elbackenddev22@gmail.com>
2025-03-08 20:11:18 -0600

    Ejercicio uno de arrays

---
+++ Retos/arrays.js
@@ -0,0 +1,11 @@
+/* 
+1.- Crea un array vacío llamado frutas
+2.- Usa el método push para agregar las siguientes frutas: manzana, banana y naranja
+3.- Usa el método pop para eliminar la última fruta del array
+*/
+
+let frutas = [];
+frutas.push("manzana", "banana", "naranja");
+frutas.pop();
+
+console.log(frutas);
