ee19ce10baee15acbaed0b9fc96394995810b308
Omega <elbackenddev22@gmail.com>
2025-03-10 16:28:09 -0600

    Funciones de orden superior

---
+++ Javascript_ECMA/function_orderS.js
@@ -0,0 +1,12 @@
+// Podemos tener funciones que cambien a otras funciones
+
+function ruidosa(funcion) {
+    return (...argumentos) => {
+        console.log("llamando con", argumentos);
+        let resultado = funcion(...argumentos);
+        console.log("llamada con", argumentos, ", retorno", resultado);
+        return resultado;
+    };
+}
+
+ruidosa(Math.min)(3, 2, 1);
