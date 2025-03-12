f7aa6637b7a49b1c7c20f3f40d0c9559b3eebda1
Omega <elbackenddev22@gmail.com>
2025-03-11 21:20:13 -0600

    Método Map

--- Javascript_ECMA/map.js
+++ Javascript_ECMA/map.js
@@ -1,7 +1,13 @@
-function map(array, transformar) {
-    let mapeados = [];
-    for (let elemento of array) {
-        mapeados.push(transformar[elemento]);
-    }
-    return mapeados;
-}
+/*
+    Se usa para transformar cada elemento de un array
+    Retorna un nuevo array con los elementos modificados
+    Ideal cuando queremos modificar valores sin cambiar el array original
+*/
+
+const numeros = [];
+numeros.push(1, 2, 3, 4, 5);
+const duplicados = numeros.map(num => num * 2);
+
+console.log(duplicados);
+
+// Cada número del array original se multiplica por 2, generando un nuevo array con los valores transformados
