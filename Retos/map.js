06967c76ca30d764f1639430ed88268c952e3763
Omega <elbackenddev22@gmail.com>
2025-03-12 19:20:40 -0600

    Ejercicio con Map - Nombres a mayúsculas

---
+++ Retos/map.js
@@ -0,0 +1,7 @@
+// Convierte un array de nombres en mayúsculas
+
+const nombres = [];
+nombres.push("ana", "carlos", "elena", "josé");
+const nombresAMayusculas = nombres.map(names => names.toUpperCase());
+
+console.log(nombresAMayusculas);
