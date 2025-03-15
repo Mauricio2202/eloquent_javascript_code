ddb5790814485040f80f7676b4d920590eacb879
Omega <elbackenddev22@gmail.com>
2025-03-15 17:00:41 -0600

    Otra forma de hacer métodos

---
+++ OOP/methods-2.js
@@ -0,0 +1,9 @@
+function hablar(linea) {
+    console.log(`El conejo '${this.tipo}' dice '${linea}'`);
+}
+
+let conejoBlanco = { tipo: "blanco", hablar };
+let conejoHambriento = { tipo: "hambriento", hablar };
+
+conejoBlanco.hablar("Oh mis orejas y bigotes, " + "qué tarde se está haciendo!");
+conejoHambriento.hablar("Podría hacerme una zanahoria ahora mismo.");
