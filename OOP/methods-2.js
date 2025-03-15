40651d4cb0e18966f94e2f1be8489a99df633432
Omega <elbackenddev22@gmail.com>
2025-03-15 17:02:50 -0600

    Método call para "llamar"

--- OOP/methods-2.js
+++ OOP/methods-2.js
@@ -7,3 +7,5 @@ let conejoHambriento = { tipo: "hambriento", hablar };
 
 conejoBlanco.hablar("Oh mis orejas y bigotes, " + "qué tarde se está haciendo!");
 conejoHambriento.hablar("Podría hacerme una zanahoria ahora mismo.");
+
+hablar.call(conejoHambriento, "Burp!");
